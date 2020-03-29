01de4cda0461ce272cb1e8c1d3b68599cfc8c9c4
William Yeung <williamyeung1141998@gmail.com>
2020-03-29 15:42:52 -0400

    Conformed to collection view protocols and working ui

--- MatchApp/Controller/ViewController.swift
+++ MatchApp/Controller/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+
     let model = CardModel()
     var cardsArray: [Card]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         cardsArray = model.getCards()
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
-
+    // MARK: - CollectionView Delegate Methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        return cell
+    }
 }
 
