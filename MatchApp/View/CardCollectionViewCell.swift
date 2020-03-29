cbb2889e23eb4c164b9fff36ae339f334cd62fa6
William Yeung <williamyeung1141998@gmail.com>
2020-03-29 14:54:48 -0400

    Fixed generateCards methods and created a custom collection view cell

---
+++ MatchApp/View/CardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchApp
+//
+//  Created by William Yeung on 3/29/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
